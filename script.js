0b09fa0d2288f05fe3a07cb314751d8d3d1be426
litox06 <litox06@gmail.com>
2022-08-10 13:33:31 -0400

    Minor additions to the messages and adding the replay game functionality

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  // When there's no input
   if (!guess) {
     document.querySelector(".message").textContent = "No number!";
+    // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "Correct number!";
+    document.querySelector(".message").textContent = "🎉 Correct number!";
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.Width = "30rem";
   } else if (guess > secretNumber) {
+    // When guess is too high
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
       score--;
@@ -23,6 +29,7 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "💥 You lost the game!";
       document.querySelector(".score").textContent = 0;
     }
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too low!";
@@ -33,4 +40,16 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = ""; // Use value instead of textContent, that's the keyword to manipulate the input's content
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.Width = "15rem";
 });
